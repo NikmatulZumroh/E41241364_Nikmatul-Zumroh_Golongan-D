1bd433dc9de82b4b63495fff912286b5f9415681
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-12 15:18:16 +0700

    Update prosedurkerja_scanner.java

--- prosedurkerja/prosedurkerja_scanner.java
+++ prosedurkerja/prosedurkerja_scanner.java
@@ -11,12 +11,12 @@ import java.util.Scanner;
 public class prosedurkerja_scanner {
     public static void main(String[] args) {
         Scanner s1 = new Scanner (System.in);
-        System.out.println("Maukkan nilai1 = ");
+        System.out.print("Maukkan nilai1 = ");
         int nilai1 = s1.nextInt();
-        System.out.println("Maukkan nilai2 = ");
+        System.out.print("Maukkan nilai2 = ");
         int nilai2 = s1.nextInt();
         
         int jumlah = nilai1 + nilai2;
-        System.out.println("Jumlah = " + jumlah);
+        System.out.print("Jumlah = " + jumlah);
     }
 }
