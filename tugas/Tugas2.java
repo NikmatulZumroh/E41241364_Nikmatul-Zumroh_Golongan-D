2a6ff50a57561234181ec5adb5dcc77069edb2d3
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-13 13:39:10 +0700

    Create Tugas2.java

---
+++ tugas/Tugas2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+public class Tugas2{
+    String name;
+    int age;
+public Tugas2(String name, int age){
+    this.name = name;
+    this.age = age;
+} 
+public String getname(){
+ return this.name;   
+}
+public int getage(){
+    return this.age;
+}
+}
+
+
+
