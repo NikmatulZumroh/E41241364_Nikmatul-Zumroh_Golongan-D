d4b09e07c8d593f74952315a51896ac66c9ecd92
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-13 13:39:18 +0700

    Create Tugas3_2.java

---
+++ tugas/Tugas3_2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class Tugas3_2 {
+    public static void main(String[] args) {
+        int tobeOrNottobe;
+        float priceIndex;
+        double lastYearsPrice;
+        long classType;
+    }
+    
+}
