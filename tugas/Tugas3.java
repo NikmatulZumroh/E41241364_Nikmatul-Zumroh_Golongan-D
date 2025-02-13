494abfcb2daee8453c4e8c6afc67ae5136352e98
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-13 13:39:14 +0700

    Create Tugas3.java

---
+++ tugas/Tugas3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class Tugas3 {
+    public static void main(String[] args) {
+        boolean gameOver = false;
+        int student = 50, classes = 3;
+        double salesTax; //Lebih baik menggunakan camelCase : salesTax bukan sales_tax
+        short number1;
+        
+        // Contoh penggunaan variable (opsional)
+        System.out.println("Game Over : "+gameOver);
+        System.out.println("Jumlah Siswa : "+student);
+        System.out.println("jumlah Kelas : "+classes);
+        //System.out.println("Pajak Penjualan : "+salesTax); // Nilai belum diinisialisasi
+        //System.out.println("Bilangan 1 : "+number1);//Nilai belum diinisialisasi
+    }
+}
