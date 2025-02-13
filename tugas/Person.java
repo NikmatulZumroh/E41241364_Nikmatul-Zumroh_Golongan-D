3d744c9235b58305453e2a466344905b0303c4e8
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-13 13:39:01 +0700

    Create Person.java

---
+++ tugas/Person.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class Person {
+    String name;
+    int age;
+    
+   public Person(String name, int age) {
+       this.name = name;
+       this.age = age;
+   }
+
+   public String getName() {
+       return this.name;
+   }
+
+   public int getAge() {
+       return this.age; }
+    }
+
