67d30a7ccd5e25c0960333e8f5536dbd19a21e95
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-13 13:39:06 +0700

    Create Tugas1.java

---
+++ tugas/Tugas1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class Tugas1 {
+    public static void main(String[] args) {
+        String fName = "Lisa";
+        String stuld = "123456789";
+        String stuStatus = "Active";
+        
+        System.out.println("Student Name : "+fName);
+        System.out.println("Student ID : "+stuld);
+        System.out.println("Student Status : "+stuStatus);
+    }
+    
+}
