3b784c64f842d83c1c1012525b3ce2408bb728d1
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-26 13:24:03 +0700

    Create Dowhile.java

---
+++ Acara9dan10/Dowhile.java
@@ -0,0 +1,24 @@
+package Acara9dan10;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class Dowhile {
+    public static void main(String[] args) {
+        //Membuat variable
+        int i = 0;
+        
+        do{
+            System.out.println("Perulangan ke -"+i);
+         i++;
+        } while (i <=10);
+        }
+    }
+    
+
