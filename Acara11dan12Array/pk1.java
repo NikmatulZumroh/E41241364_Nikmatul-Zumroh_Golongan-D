9c03a3d5ceea9800d5798b456f078d2756ec1e79
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-26 13:26:12 +0700

    Create pk1.java

---
+++ Acara11dan12Array/pk1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Acara11dan12Array;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class pk1 {
+    public static void main(String[] args) {
+        int[] angka = {3,7,8,9,10,11,12};
+        
+        System.out.println("Jumlah elemen array : "+angka.length );
+    }
+}
