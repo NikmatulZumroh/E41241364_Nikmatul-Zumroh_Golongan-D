243d9b45862c1c2229c7a1711e48aee44852c33c
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-26 13:22:39 +0700

    Create tugas2.java

---
+++ Acara9dan10/tugas2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Acara9dan10;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class tugas2 {
+    public static void main(String[] args) {
+        System.out.println("       Do - While       ");
+        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("============================");        
+        int i = 1;            
+        do{
+            System.out.print(i+",");
+            i *= 2;
+        } while (i <= 100);
+        System.out.println("");
+    }
+}
