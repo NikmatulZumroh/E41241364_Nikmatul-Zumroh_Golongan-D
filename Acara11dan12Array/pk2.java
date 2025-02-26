31d8356e85b5d11cd3f4db6602a05585cf5b5fcd
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-26 13:26:09 +0700

    Create pk2.java

---
+++ Acara11dan12Array/pk2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Acara11dan12Array;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class pk2 {
+    public static void main(String[] args) {
+        int[] angka = {7, 10, 17, 20, 1};
+        String[] teks ={"Saya","Sedang","Belajar","OOP","Java"};
+        
+        for(int i=0; i < angka.length; i++){
+            System.out.println(angka[i]);    
+        }
+        for(int j=0;j < teks.length; j++){
+            System.out.print(teks[j]);
+            System.out.print(" ");
+        }            
+    }
+}
