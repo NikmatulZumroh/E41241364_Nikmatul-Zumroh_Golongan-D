b37d69b7167d81f956a732e17a9544ace7bf0fed
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-02-26 13:23:54 +0700

    Create For_Each.java

---
+++ Acara9dan10/For_Each.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Acara9dan10;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class For_Each {
+    public static void main(String[] args) {
+        // Membuat array
+        int angka[] = {3,1,42,24,12};
+        // menggunakan perulangan for each untuk menampilkan angka
+        for (int z : angka){
+            System.out.print(z + " ");
+        }
+    }
+}
