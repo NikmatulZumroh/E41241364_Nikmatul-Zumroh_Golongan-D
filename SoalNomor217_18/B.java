e8b9d61b7b6fb5342aee4fdacbff91a6988bb490
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-03-09 14:49:55 +0700

    Create B.java

---
+++ SoalNomor217_18/B.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SoalNomor217_18;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class B extends A{
+    void callthis(){
+        System.out.println("Inside class B's Method!");
+    }
+}
