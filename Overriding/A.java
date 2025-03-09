f75fe4cc9daafeece8c7818483b6d5d4fbe2c4a8
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-03-09 14:48:57 +0700

    Create A.java

---
+++ Overriding/A.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class A {
+    public void tampilkanKelayar(){
+        System.out.println("Method milik class A dipanggil.");
+    }
+}
