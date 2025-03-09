de70f1e8dc96bcb63ac711e70a69e172d1d1ad05
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-03-09 14:48:17 +0700

    Create A.java

---
+++ Abstract/A.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Abstract;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public abstract class A { // Deklaraasikan Class
+    abstract public void method2(); // Deklarasi method abstrak.
+    // method konkrit yang memiliki implementasi
+    public void method1(){
+        System.out.println("Method konkrit dari class A");
+    }
+}
