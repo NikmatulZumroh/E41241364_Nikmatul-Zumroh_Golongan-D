c9fdba0d1ca066c649f24c3e55a713db543ecec4
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-03-09 14:49:07 +0700

    Create A.java

---
+++ Pewarisan/A.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pewarisan;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class A {
+    int x;
+    int y;
+    
+    void TampiilanNilaiXY(){
+        System.out.println("Nilai X : " + x + ", Y : " + y);
+    }
+}
