6442cca3819e7fcc4816d07f0c30a7d0cd3b8805
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-03-09 14:51:20 +0700

    Create Animal.java

---
+++ Nomor3Acara17_18/Animal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Nomor3Acara17_18;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class Animal {
+    public void makeNoise(){
+        System.out.println("Talk");
+    }
+    }
+
