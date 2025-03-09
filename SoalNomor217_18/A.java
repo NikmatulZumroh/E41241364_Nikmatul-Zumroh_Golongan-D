b9e8c7d67768f6b57ed1276b21cf9a1962377ce4
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-03-09 14:49:50 +0700

    Create A.java

---
+++ SoalNomor217_18/A.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SoalNomor217_18;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class A {
+    void callthis(){
+        System.out.println("Inside Class A's Method!");
+    }
+}
