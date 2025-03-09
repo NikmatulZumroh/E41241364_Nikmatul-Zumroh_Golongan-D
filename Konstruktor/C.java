e3978526d25d347d42dbcfa589701dec03f30959
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-03-09 14:48:49 +0700

    Create C.java

---
+++ Konstruktor/C.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Konstruktor;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class C extends B{
+   C(){ // super() : Tambahkan kode secara otomatis oleh Java untuk memanggil Konstruktor dari SuperClassnya
+         //           Konstruktor dari SuperClassnya
+         
+         super(); // Konstruktor default dari class
+                  // Object dipanggilS
+       System.out.println("Konstruktor class C dieksekusi ...");
+   } 
+}
