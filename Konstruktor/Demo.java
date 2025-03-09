8e660350f866f7c2c6a540eb65e9b42e5976caa5
NikmatulZumroh <zumrohnikma458@gmail.com>
2025-03-09 14:48:54 +0700

    Create Demo.java

---
+++ Konstruktor/Demo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Konstruktor;
+
+/**
+ *
+ * @author Nikmatul Zumroh
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B subOb = new B();
+    }
+ 
+}
